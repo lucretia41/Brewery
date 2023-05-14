c8b42cc10316938bcc2f2d94217cf2128cc290d7
Ian Scott Martin <ianscottmartin@gmail.com>
2023-05-13 23:04:43 -0600

    tweeks on fetch

--- main.js
+++ main.js
@@ -3,9 +3,16 @@ const init = () => {
   
     inputForm.addEventListener("submit", (event) => {
       event.preventDefault();
-      fetch("http://localhost:3000/movies")
+      const input = document.querySelector('input#searchByID')
+
+      fetch(`http://localhost:3000/movies/${input.value}`)
       .then((response) => response.json())
       .then((data) => {
+        const title = document.querySelector("section#titleDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
         console.log(data);
         // LOG: (3) [{…}, {…}, {…}]
 
@@ -14,3 +21,8 @@ const init = () => {
   };
   
   document.addEventListener("DOMContentLoaded", init);
+
+  const movielist = document.createElement("li")
+  
+//   document.querySelector('#movieDetails').append(section)
+ 
---
+++ scracth.js
@@ -0,0 +1,91 @@
+
+{ "beer" [
+    
+    {
+        "id": "1",
+        "name": "Running Dogs Brewery",
+        "hefeweizen": "yes",
+        "ipa": "yes",
+        "pilsner": "yes"
+
+    },
+    {
+        "id": "2",
+        "name": "Sea Dog Brewing",
+        "hefeweizen": "yes",
+        "ipa": "yes",
+        "pilsner": "yes"
+    },
+    {
+        "id": "3",
+        "name": "Barrel Dog Brewing",
+        "hefeweizen": "yes",
+        "ipa": "yes",
+        "pilsner": "yes"
+    },
+    {
+        "id": "4",
+        "name": "Dreaming Dog Brewery",
+        "hefeweizen": "yes",
+        "ipa": "yes",
+        "pilsner": "yes"
+    },
+    {
+        "id": "5",
+        "name": "Lead Dog Brewing",
+        "hefeweizen": "yes",
+        "ipa": "yes",
+        "pilsner": "yes"
+    },
+    {
+        "id": "6",
+        "name": "Tin Dog Brewing",
+        "hefeweizen": "yes",
+        "ipa": "yes",
+        "pilsner": "yes"
+    },
+    {
+        "id": "7",
+        "name": "Dog Days Brewing",
+        "hefeweizen": "yes",
+        "ipa": "yes",
+        "pilsner": "yes"
+    },
+    {
+        "id": "8",
+        "name": "Boss Dog Brewing",
+        "hefeweizen": "yes",
+        "ipa": "yes",
+        "pilsner": "yes"
+    },
+    {
+        "id": "9",
+        "name": "Flying Dog Brewery",
+        "hefeweizen": "yes",
+        "ipa": "yes",
+        "pilsner": "yes"
+    },
+    {
+        "id": "10",
+        "name": "Dog Tag Brewing",
+        "hefeweizen": "yes",
+        "ipa": "yes",
+        "pilsner": "yes"
+    },
+    {
+        "id": "11",
+        "name": "Laughing Dog Brewing",
+        "hefeweizen": "yes",
+        "ipa": "yes",
+        "pilsner": "yes"
+
+    },
+    {
+        "id": "12",
+        "name": "Dog Money Restaurant",
+        "hefeweizen": "yes",
+        "ipa": "yes",
+        "pilsner": "yes"
+    }
+]
+}
